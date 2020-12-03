0e2db346ef26e8a6bcd9833ce8d953369e354341
Vishnu ES <55495318+Vishnu-es@users.noreply.github.com>
2020-12-03 18:23:11 +0530

    Featured courses

--- lib/Screens/HomeScreens/Featured.dart
+++ lib/Screens/HomeScreens/Featured.dart
@@ -1,7 +1,12 @@
+import 'dart:ui';
+
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
+import 'package:udemyclone/Screens/MyList.dart';
 import 'package:udemyclone/Services/DataController.dart';
 
 class Featured extends StatefulWidget {
@@ -16,9 +21,13 @@ class _FeaturedState extends State<Featured> {
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Text("Featured"),
+        title: Text(""),
         actions: [
-          IconButton(icon: Icon(Icons.add_shopping_cart), onPressed: () {}),
+          IconButton(
+              icon: Icon(Icons.add_shopping_cart),
+              onPressed: () {
+                Get.to(MyList());
+              }),
         ],
       ),
       body: SingleChildScrollView(
@@ -27,17 +36,36 @@ class _FeaturedState extends State<Featured> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Brighten your future",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                      Text(
+                        "Explore new interests",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ],
+                  ),
+                ),
                 height: 180,
                 width: 400,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
                   image: DecorationImage(
-                      image: AssetImage("images/sales.png"), fit: BoxFit.cover),
+                      image: AssetImage("images/salesbg.jpg"),
+                      fit: BoxFit.cover),
                 )),
             Padding(
               padding: const EdgeInsets.only(top: 12.0),
               child: Container(
-                width: MediaQuery.of(context).size.width - 10,
+                width: MediaQuery.of(context).size.width - 2,
                 height: 65,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
@@ -67,10 +95,13 @@ class _FeaturedState extends State<Featured> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.only(top: 22, left: 10.0, bottom: 12),
               child: Text(
                 "Featured",
-                style: TextStyle(color: Colors.white, fontSize: 17),
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                    fontWeight: FontWeight.w800),
               ),
             ),
             SizedBox(
@@ -93,25 +124,182 @@ class _FeaturedState extends State<Featured> {
                             scrollDirection: Axis.horizontal,
                             itemCount: snapshot.data.length,
                             itemBuilder: (BuildContext context, int index) {
-                              return Container(
-                                child: Column(
-                                  children: [
-                                    Container(
-                                      height: 200,
-                                      width: 200,
-                                      decoration: BoxDecoration(
-                                          image: DecorationImage(
-                                              fit: BoxFit.contain,
-                                              image: NetworkImage(snapshot
-                                                  .data[index]
-                                                  .data()['image']))),
-                                    ),
-                                    Container(),
-                                    Text(
-                                      snapshot.data[index].data()['title'],
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                  ],
+                              return Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 10, right: 12),
+                                child: Container(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        height: 100,
+                                        width: 180,
+                                        decoration: BoxDecoration(
+                                            image: DecorationImage(
+                                                fit: BoxFit.contain,
+                                                image: NetworkImage(snapshot
+                                                    .data[index]
+                                                    .data()['image']))),
+                                      ),
+                                      Container(
+                                        constraints:
+                                            BoxConstraints(maxWidth: 180),
+                                        child: Padding(
+                                          padding:
+                                              const EdgeInsets.only(top: 10.0),
+                                          child: snapshot.data[index]
+                                                      .data()['title']
+                                                      .length <
+                                                  47
+                                              ? Text(
+                                                  snapshot.data[index]
+                                                          .data()['title']
+                                                          .substring(0, 40) +
+                                                      "...",
+                                                  style: TextStyle(
+                                                      color: Colors.white),
+                                                )
+                                              : Text(
+                                                  snapshot.data[index]
+                                                          .data()['title']
+                                                          .substring(0, 42) +
+                                                      "...",
+                                                  style: TextStyle(
+                                                      color: Colors.white),
+                                                ),
+                                          // Text(
+                                          //     snapshot.data[index]
+                                          //             .data()['title']
+                                          //             .substring(0, 48) +
+                                          //         "...",
+                                          //     style:
+                                          //         TextStyle(color: Colors.white),
+                                          //   ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(
+                                            top: 8.0, bottom: 8.0),
+                                        child: Text(
+                                          snapshot.data[index].data()['author'],
+                                          style: TextStyle(
+                                              color: Colors.grey, fontSize: 13),
+                                        ),
+                                      ),
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            EvaIcons.star,
+                                            size: 16,
+                                            color: Colors.orangeAccent,
+                                          ),
+                                          Icon(
+                                            EvaIcons.star,
+                                            size: 16,
+                                            color: Colors.orangeAccent,
+                                          ),
+                                          Icon(
+                                            EvaIcons.star,
+                                            size: 16,
+                                            color: Colors.orangeAccent,
+                                          ),
+                                          Icon(
+                                            EvaIcons.star,
+                                            size: 16,
+                                            color: Colors.orangeAccent,
+                                          ),
+                                          Icon(
+                                            EvaIcons.star,
+                                            size: 16,
+                                            color: Colors.orangeAccent,
+                                          ),
+                                          Text(
+                                            snapshot.data[index]
+                                                .data()['ratings'],
+                                            style: TextStyle(
+                                                color: Colors.grey,
+                                                fontSize: 13),
+                                          ),
+                                          Text(
+                                            ('(${snapshot.data[index].data()['enrolled']})'),
+                                            style: TextStyle(
+                                                color: Colors.grey,
+                                                fontSize: 13),
+                                          ),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          Text(
+                                            snapshot.data[index]
+                                                .data()['price'],
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 15),
+                                          ),
+                                          SizedBox(
+                                            width: 20,
+                                          ),
+                                          Text(
+                                            snapshot.data[index]
+                                                .data()['notPrice'],
+                                            style: TextStyle(
+                                                color: Colors.white70,
+                                                decoration:
+                                                    TextDecoration.lineThrough,
+                                                fontSize: 12),
+                                          ),
+                                        ],
+                                      ),
+                                      snapshot.data[index].data()['tag'] ==
+                                              "Bestseller"
+                                          ? Container(
+                                              height: 25,
+                                              width: 80,
+                                              decoration: BoxDecoration(
+                                                  color: Colors
+                                                      .orangeAccent.shade100,
+                                                  borderRadius:
+                                                      BorderRadius.circular(3)),
+                                              child: Padding(
+                                                padding: const EdgeInsets.only(
+                                                    top: 3.0,
+                                                    left: 3.0,
+                                                    right: 3.0),
+                                                child: Text(
+                                                  snapshot.data[index]
+                                                      .data()['tag'],
+                                                  style: TextStyle(
+                                                      color: Colors.black,
+                                                      fontSize: 15),
+                                                ),
+                                              ),
+                                            )
+                                          : Container(
+                                              height: 25,
+                                              width: 110,
+                                              decoration: BoxDecoration(
+                                                  color: Colors
+                                                      .deepOrange.shade300,
+                                                  borderRadius:
+                                                      BorderRadius.circular(3)),
+                                              child: Padding(
+                                                padding: const EdgeInsets.only(
+                                                    top: 3.0,
+                                                    left: 3.0,
+                                                    right: 3.0),
+                                                child: Text(
+                                                  snapshot.data[index]
+                                                      .data()['tag'],
+                                                  style: TextStyle(
+                                                      color: Colors.black,
+                                                      fontSize: 15),
+                                                ),
+                                              ),
+                                            )
+                                    ],
+                                  ),
                                 ),
                               );
                             });
@@ -120,7 +308,17 @@ class _FeaturedState extends State<Featured> {
                   );
                 },
               ),
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 0, left: 10.0, bottom: 12),
+              child: Text(
+                "Top courses in Development",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                    fontWeight: FontWeight.w800),
+              ),
+            ),
           ],
         ),
       ),
